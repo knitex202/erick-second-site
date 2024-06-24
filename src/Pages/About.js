26ef0c654ecdc643df2f7441200eaeabd5921b3b
knitex202 <99418580+knitex202@users.noreply.github.com>
2024-06-23 17:35:17 -0700

    fixed custom theme responsiveness

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,8 +3,8 @@ import aboutImage from "../Assets/galleryImages/aboutImage.jpg";
 
 function About() {
   return (
-    <div className="flex flex-col w-screen h-full overflow-x-hidden">
-      <div className="flex flex-col w-full short:h-96 h-screen overflow-hidden">
+    <div className="flex flex-col w-screen h-full overflow-hidden">
+      <div className="flex flex-col w-full short:h-[600px] h-screen overflow-hidden">
         <div className="flex flex-col z-40 w-full absolute ml-4 top-32 text-white xl:text-2xl gap-2 xl:end-24 xl:items-end">
           <p>HEIGHT: <span className="font-semibold">5'10"/178cm</span></p>
             <p>NECK: <span className="font-semibold">15½</span></p>
